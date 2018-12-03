ce8d75215cc99605f587bd65166bab4771c780b8
alchmst <jerezorayle@gmail.com>
2018-12-03 15:21:01 +0800

    Cryptobot using Telegram Bot API (Updated 120318)

--- src/TG/Commands/MiscellaneusCommands.java
+++ src/TG/Commands/MiscellaneusCommands.java
@@ -12,38 +12,45 @@ public class MiscellaneusCommands extends BotCommand {
 
     @Override
     public void command(Message message, String txt, SendMessage reply) {
-        String[] tokenMsg = txt.split(" ");
+        String[] command = txt.split(" ");
 
-        switch (tokenMsg[0].toLowerCase()) {
-            case "hello":
-                reply.setText("Hi.");
-                break;
-            case "hi":
-                reply.setText("Hello.");
-                break;
-            case "/reverse":
-                reply.setText((new StringBuffer(txt.substring(9))).reverse().toString());
-                break;
-            case "/date":
-                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-                LocalDateTime now = LocalDateTime.now();
-                reply.setText(dtf.format(now));
-                break;
-            case "/id":
-                reply.setText("Your chat ID is: " + message.getChatId());
-                break;
-            case "/fname":
-                reply.setText("Your first name is " + message.getChat().getFirstName() + ".");
-                break;
-            case "/lname":
-                reply.setText("Your last name is " + message.getChat().getLastName() + ".");
-                break;
-            case "/help":
-                reply.setText("/help\tget commands\n/id\tchat id\n/price [crypto_currency] [currency]\tget crypto currency rates\n/reverse [msg]");
-                break;
-            default:
-                break;
+        if (txt.toLowerCase().contains("your name")) {
+            reply.setText("My name is Cryptobot.");
+        }
+        if (txt.toLowerCase().matches("\bhi\b")) {
+            reply.setText("Hello.");
+        }
+        if (txt.toLowerCase().matches("\bhello\b")) {
+            reply.setText("Hi.");
+        }
 
+        try {
+            switch (command[0].toLowerCase()) {
+                case "/reverse":
+                    reply.setText((new StringBuffer(command[1])).reverse().toString());
+                    break;
+                case "/date":
+                    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+                    LocalDateTime now = LocalDateTime.now();
+                    reply.setText(dtf.format(now));
+                    break;
+                case "/id":
+                    reply.setText("Your chat ID is: " + message.getChatId());
+                    break;
+                case "/fname":
+                    reply.setText("Your first name is " + message.getChat().getFirstName() + ".");
+                    break;
+                case "/lname":
+                    reply.setText("Your last name is " + message.getChat().getLastName() + ".");
+                    break;
+                case "/help":
+                    reply.setText("/help\tget commands\n/id\tchat id\n/price [crypto_currency] [currency]\tget crypto currency rates\n/reverse [msg]");
+                    break;
+                default:
+                    break;
+            }
+        } catch (Exception e) {
+            reply.setText("Argument invalid. Type /help for list of commands.");
         }
     }
 }
